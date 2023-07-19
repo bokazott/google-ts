af29c148f1f0839e91d70295b1bb48376814efe5
bokazott <64536239+bokazott@users.noreply.github.com>
2023-07-19 19:59:10 +0200

    Static Google Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Image from "C:/Users/PC/Desktop/typescript/google-ts/src/images/gooogle.jpg";
 
 function App() {
-  return (
-    <div className="App">
-    <h1>Hello world</h1>
-      <button className="btn btn-primary">Click</button>
-    </div>
-  );
+    return (
+        <div className="container text-center">
+            <div className="row align-items-center mt-5">
+                <div className="col"></div>
+                <div className="col-6">
+                    <img style={nest} src={Image} alt="Google slika"/>
+                    <form>
+                        <input className="form-control mt-4" type="text" placeholder="Search Here"/>
+                    </form>
+                    <div className="row mt-3">
+                        <div className="col-2"></div>
+                        <div className="col-8 btn-toolbar justify-content-between">
+                            <button type="button" className="btn btn-light">Google Search</button>
+                            <button type="button" className="btn btn-light">Im feeling lucky</button>
+                        </div>
+                        <div className="col-2"></div>
+                    </div>
+                </div>
+                <div className="col"></div>
+            </div>
+
+
+        </div>
+    );
+}
+
+let nest = {
+    width : '70%'
 }
 
 export default App;
+
